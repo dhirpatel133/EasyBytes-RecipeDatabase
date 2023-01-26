7062a7692370a3fe122d25c4e4ba4ecb76d1779f
Soham Shah <soham@Sohams-MacBook-Pro.local>
2023-01-25 21:56:14 -0500

    Created backend db connection, and create frontend and backend connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+
+
+let url = 'http://localhost:5000/'; 
+
 
 const App = () => {
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    );
+        let url = 'http://localhost:5000/'; 
+        const [data, setData] = useState("");
+      
+        const subscribeMe = async (event) => {
+          const res = await fetch(url);    
+          const data = await res.json();
+          console.log(data);
+          const outputData = data;
+          setData(outputData);
+        };
+        
+        return (
+          <div className="App">
+            <h1>Welcome to EasyByte</h1>
+            <h2>Click the Button Below to Retrive Data from a MySQL Database!</h2>
+            <button onClick={subscribeMe}>Get Data</button>
+            <br />
+            <span>
+            {" "}
+            <div>
+                {data === "" ? <h1> </h1> : 
+                <div><h3>Here is a list of Students in the Student Table</h3>
+                <ol>
+                {data.map(name => (
+                    <li key={name}>{name['name']}</li>
+                ))}
+            </ol></div>}
+            </div>
+            </span>
+          </div>
+        );
+
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,40 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import mysql2 from 'mysql2'
 import cors from 'cors';
 
 const app = express()
 const port = 5000
 
+app.use(cors())
+
+const connection = mysql2.createConnection({
+  host: 'localhost',
+  port: 3306,
+  database: 'sample_db',
+  user: 'root',
+  password: 'sonu@2002'
+});
+
+connection.connect(function (err) {
+  if(err){
+      console.log("error occurred while connecting");
+      console.log(err)
+  }
+  else{
+      console.log("connection created with Mysql successfully");
+  }
+});
+
 app.get('/', (req, res) => {
-  res.send('Backend: Hello World!')
+  //res.send('Backend: Hello World!')
+  connection.query(
+    'SELECT name FROM student',
+    function(err, results, fields) {
+      res.send(results); // results contains rows returned by server
+    }
+  );
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
