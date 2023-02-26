329430b7a69356d51e651f8def7cfda739c250a2
Soham Shah <soham@Sohams-MacBook-Pro.local>
2023-02-26 00:06:32 -0500

    finished backend routes for milestone 1

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import mysql2 from 'mysql2'
 import cors from 'cors';
-import fs from 'fs'
-import { parse } from 'csv-parse';
-import readline from 'readline'
-import { populateTables } from './sample-populate-db.js';
 
 //Bcrypt
+
+function generateRandomUUID(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 const connection = mysql2.createConnection({
   host: 'localhost',
@@ -26,7 +26,7 @@ connection.connect(function (err) {
   }
 });
 
-populateTables(connection);
+//populateTables(connection);
 
 const app = express()
 const port = 5000
@@ -34,35 +34,121 @@ const port = 5000
 app.use(cors())
 
 app.get('/', (req, res) => {
+  res.send('Welcome to EasyBytes API!')
+})
+
+app.get('/getAllPosts', (req, res) => {
   //res.send('Backend: Hello World!')
   connection.query(
-    'SELECT name, cuisine FROM recipes',
-    function(err, results, fields) {
+    'SELECT * FROM recipes JOIN posts ON recipes.recipe_id = posts.recipe_id',
+    function(err, results) {
       res.send(results); // results contains rows returned by server
     }
   );
 })
 
-app.get('/hi', (req, res) => {
+// INSERT INTO recipes VALUE (recipe_id, name, cuisine, cook_time, ingredients, instructions, calories, meal_type, health_label, health_score, servings, picture)
+
+app.get('/test', (req, res) => {
   //res.send('Backend: Hello World!')
   connection.query(
     'SHOW tables',
+    function(err, results) {
+      res.send(results); // results contains rows returned by server
+    }
+  );
+})
+
+// http://localhost:5000/getCustomPosts?filterCategory=cuisine&filterValue=Italian
+// http://localhost:5000/getCustomPosts?sortCategory=calories&sortValue=desc
+app.get('/getCustomPosts', (req, res) => {
+  
+  let filterCategory = req.query.filterCategory
+  let filterValue = req.query.filterValue
+  let sortCategory = req.query.sortCategory
+  let sortValue = req.query.sortValue
+  let filterOnlyQuery = `SELECT * FROM recipes JOIN posts ON recipes.recipe_id = posts.recipe_id WHERE ${filterCategory} LIKE '%${filterValue}%'`
+  let sortOnlyQuery = `SELECT * FROM recipes JOIN posts ON recipes.recipe_id = posts.recipe_id ORDER BY ${sortCategory} ${sortValue}`
+  let filterAndSortQuery = `SELECT * FROM recipes JOIN posts ON recipes.recipe_id = posts.recipe_id WHERE ${filterCategory} LIKE '%${filterValue}%' ORDER BY ${sortCategory} ${sortValue}`
+  let finalQuery;
+  
+  if (filterCategory && filterValue && sortCategory === undefined && sortValue === undefined) {
+    finalQuery = filterOnlyQuery
+  }
+  else if (filterCategory === undefined && filterValue === undefined && sortCategory && sortValue) {
+    finalQuery = sortOnlyQuery
+  }
+  else if (filterCategory && filterValue && sortCategory && sortValue) {
+    finalQuery = filterAndSortQuery
+  }
+
+  connection.query(
+    finalQuery,
     function(err, results, fields) {
       res.send(results); // results contains rows returned by server
     }
   );
 })
 
-/*app.get('/test', (req, res) => {
-  //res.send('Backend: Hello World!')
-  fs.createReadStream("../db/sample-data/post-sample.csv")
-    .pipe(parse({ delimiter: ",", from_line: 2 }))
-    .on("data", function (row) { 
+let currentTime = new Date()
+let mySQLTime = new Date(
+  currentTime.getFullYear(),
+  currentTime.getMonth(),
+  currentTime.getDate(),
+  currentTime.getHours() - currentTime.getTimezoneOffset() / 60,
+  (currentTime.getMinutes()),
+  currentTime.getSeconds(),
+  currentTime.getMilliseconds()
+).toISOString().slice(0, 19).replace('T', ' ')
+
+app.get('/createPost', (req, res) => {
+  // put error checking
+  let recipeID = generateRandomUUID(120, 199);
+  let dishName = req.query.dishName
+  let cuisine = req.query.cuisine
+  let cookTime = req.query.cookTime
+  let ingredients = req.query.ingredients
+  let instructions = req.query.instructions
+  let calories = req.query.calories
+  let mealType = req.query.mealType
+  let healthLabel = req.query.healthLabel
+  let healthScore = req.query.healthScore
+  let servings = req.query.servings
+  let picture = req.query.picture
+
+  let postID = generateRandomUUID(220, 299);
+  let userID = req.query.userID
+  let createdAt = mySQLTime
+  let updatedAt = mySQLTime
+
+  console.log(recipeID, postID)
+  //console.log(createdAt, updatedAt)
+
+  connection.query(
+    `INSERT INTO recipes VALUE (${recipeID}, '${dishName}', '${cuisine}', ${cookTime}, '${ingredients}', '${instructions}', ${calories}, '${mealType}', '${healthLabel}', ${healthScore}, ${servings}, '${picture}');`,
+    function(err, results, fields) {
       connection.query(
-        `INSERT INTO posts VALUES (${row[0]}, ${row[1]}, ${row[2]}, '${row[3]}', '${row[4]}')`,
+        `INSERT INTO posts VALUE (${postID}, ${recipeID}, ${userID}, '${createdAt}', '${updatedAt}');`,
+        function(err, results, fields) {
+          console.log(results)
+          res.send(results)
+        }
       );
-    })
-})*/
+    }
+  );
+})
+
+app.get('/deletePost', (req, res) => {
+  
+  let post_id = req.query.postid;
+  
+  connection.query(
+    `DELETE FROM recipes WHERE recipe_id = (SELECT recipe_id FROM posts WHERE post_id = ${post_id});`,
+    function(err, results, fields) {
+      res.send(results)
+    }
+  );
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
